2c2050ea8c711ccdcf5858bbb42dc675d0bbf363
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-11-27 19:44:05 -0800

    Desolve the eventlistner nesting

--- src/modules/display-proj-form.js
+++ src/modules/display-proj-form.js
@@ -15,34 +15,36 @@ import { removeAllChildren } from '../helpers/remove-child';
 function displayProjForm() {
 
     const addProjBtn = document.getElementById('addProjBtn');
+    
     addProjBtn.addEventListener('click', ()=>{
         generateProjForm(); //generate a proj form
         displaySubmitProjBtn(); //display a submit proj button
+    })
 
-        submitProjBtn.addEventListener('click', ()=>{            
-            let indexNum = createProjObj(); //create a proj obj and push it to projArr and get an index number
-            console.log(indexNum);
-            displayAddItemBtn(); //display an add item button
-            addItemBtn.setAttribute('data-index', indexNum);
-            projFormDiv.removeChild(projForm);
+    submitProjBtn.addEventListener('click', ()=>{            
+        let indexNum = createProjObj(); //create a proj obj and push it to projArr and get an index number
+        console.log(indexNum);
+        displayAddItemBtn(); //display an add item button
+        addItemBtn.setAttribute('data-index', indexNum);
+        projFormDiv.removeChild(projForm);
+    })
 
-            addItemBtn.addEventListener('click', ()=>{
-                generateItemForm(); //generate a todo item form
-                displaySubmitItemBtn(); //display a submit item button
-                let addItemBtnIndex = addItemBtn.getAttribute('data-index');
-                submitItemBtn.setAttribute('data-index', addItemBtnIndex);
+    addItemBtn.addEventListener('click', ()=>{
+        generateItemForm(); //generate a todo item form
+        displaySubmitItemBtn(); //display a submit item button
+        let addItemBtnIndex = addItemBtn.getAttribute('data-index');
+        submitItemBtn.setAttribute('data-index', addItemBtnIndex);
+    })
 
-                submitItemBtn.addEventListener('click', ()=>{
-                let submitItemBtnIndex = submitItemBtn.getAttribute('data-index');
-                projArr[submitItemBtnIndex].createItemObj();
-                console.log(projArr);
-                console.log(projArr[submitItemBtnIndex].itemArr);
-                removeAllChildren(todoItemFormDiv);
-                displayAddItemBtn(); //display an add item button
-                })
-            })
-        })
-    });         
+    submitItemBtn.addEventListener('click', ()=>{
+    let submitItemBtnIndex = submitItemBtn.getAttribute('data-index');
+    projArr[submitItemBtnIndex].createItemObj();
+    console.log(projArr);
+    console.log(projArr[submitItemBtnIndex].itemArr);
+    removeAllChildren(todoItemFormDiv);
+    displayAddItemBtn(); //display an add item button
+    })
+
 }
 
 
