818af44ff429366cbd8624732bc6b79856275f11
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-12-08 20:10:13 -0800

    Clean code

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ import { addEventListners, addProjBtn } from './modules/add-eventlistners';
 import { createDefaultProjObj } from './modules/proj-class';
 import { displayProjArr, toggleProjs, activeProjTitleDiv }  from './modules/display-projArr';
 import { addItemBtn, displayAddItemBtn } from './modules/generate-addItem-btn';
-import { projArr } from './modules/proj-class';
 import { getProjArrFromLocalStorage } from './modules/localStorage';
-import { projArrUl } from './modules/display-projArr';
 
 //functions to invoke at page load;
 displayAddItemBtn();    
